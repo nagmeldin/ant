31c505e677cf52b75509a9b93f1c8915e8382853
nagmeldin <eldin.nagm@gmail.com>
2015-06-24 21:11:46 -0600

    added more enhancement

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
+   /*     final static Logger log = LoggerFactory.getLogger(Hello.class); */
      public static void main(String args[])
+      
               {
                 System.out.println ("Hello Ant World");
+               /* log.debug("bad things happen!"); */
                }
               }

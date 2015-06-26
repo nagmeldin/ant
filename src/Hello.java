232bb1ddfcfc8ad4f64c9ca0c097c70bdc967dd8
nagmeldin <eldin.nagm@gmail.com>
2015-06-26 17:48:14 -0600

    added some enhancements

--- src/Hello.java
+++ src/Hello.java
@@ -3,6 +3,9 @@ public class Hello {
      public static void main(String args[])
       
               {
+				  
+				 /* We need to add logging to this second version of the app (R2) */ 
+				 
                 System.out.println ("Hello Ant World");
                /* log.debug("bad things happen!"); */
                }

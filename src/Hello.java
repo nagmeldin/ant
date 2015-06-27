f65c50a7da5e826ddeaca9c29aaec7bc1170b54a
nagmeldin <eldin.nagm@gmail.com>
2015-06-26 18:29:14 -0600

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
      public static void main(String args[])
       
               {
-                System.out.println ("Hello Ant World");
+                System.out.println ("Hello Ant World")
                /* log.debug("bad things happen!"); */
                }
               }
